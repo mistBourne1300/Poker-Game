6e0af1464464177c6dc764d81a3935faa4a52c8f
Chase Ford <mistbender13@gmail.com>
2025-01-07 12:39:34 -0700

    just changed testing file

--- poker_game/test.py
+++ poker_game/test.py
@@ -9,6 +9,6 @@ from itertools import permutations
 if __name__ == "__main__":
     hand = [utils.str_to_tuple(cstr) for cstr in ['2c','qh']]
     tabled = [utils.str_to_tuple(cstr) for cstr in ['qd', '2s', '7h', 'ks']]
-    num_opps = 2
-
-    print(utils.calc_probs_multiple_opps(hand, tabled, num_opps))
+    num_opps = 6
+    np.set_printoptions(suppress=True)
+    print(utils.calc_probs_multiple_opps(hand, tabled, num_opps)*100)
